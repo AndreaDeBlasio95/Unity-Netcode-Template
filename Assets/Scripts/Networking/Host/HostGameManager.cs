d8486daaebeda8f3c6b4f68346b2a64a14553c9c
AndreaDeBlasio55 <39781342+AndreaDeBlasio95@users.noreply.github.com>
2024-09-19 12:42:06 +0200

    overhead playername + spawn points

--- Assets/Scripts/Networking/Host/HostGameManager.cs
+++ Assets/Scripts/Networking/Host/HostGameManager.cs
@@ -21,7 +21,7 @@ public class HostGameManager : IDisposable
     [SerializeField] private string joinCode;
     [SerializeField] private string lobbyId;
     [SerializeField] private const string GameSceneName = "Game";
-    private NetworkServer networkServer;
+    public NetworkServer NetworkServer { get; private set; }
 
     public async Task StartHostAsync()
     {
@@ -75,7 +75,7 @@ public class HostGameManager : IDisposable
             return;
         }
 
-        networkServer = new NetworkServer(NetworkManager.Singleton);
+        NetworkServer = new NetworkServer(NetworkManager.Singleton);
 
         UserData userData = new UserData
         {
@@ -120,6 +120,6 @@ public class HostGameManager : IDisposable
             lobbyId = string.Empty;
         }
 
-        networkServer?.Dispose();
+        NetworkServer?.Dispose();
     }
 }
