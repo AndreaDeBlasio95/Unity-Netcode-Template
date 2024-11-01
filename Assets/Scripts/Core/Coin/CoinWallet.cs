3104cb5c1f37a6de1d4f3b7bcc6926244422e76b
AndreaDeBlasio55 <39781342+AndreaDeBlasio95@users.noreply.github.com>
2024-11-01 23:50:04 +0100

    .

--- Assets/Scripts/Core/Coin/CoinWallet.cs
+++ Assets/Scripts/Core/Coin/CoinWallet.cs
@@ -5,7 +5,40 @@ using UnityEngine;
 
 public class CoinWallet : NetworkBehaviour
 {
+    [Header("References")]
+    [SerializeField] private Health health;
+    [SerializeField] private BountyCoin coinPrefab;
+
+    [Header("Settings")]
+    [SerializeField] private float coinSpread = 3.0f;
+    [SerializeField] private float bountyPercentage = 50.0f;
+    [SerializeField] private int bountyCoinCount = 10;
+    [SerializeField] private int minBountyCoinValue = 5;
+
+    private Collider2D[] coinBuffer = new Collider2D[1];
+    private float coinRadius;
+
     public NetworkVariable<int> TotalCoins = new NetworkVariable<int>();
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsServer) { return; }
+
+        coinRadius = coinPrefab.GetComponent<CircleCollider2D>().radius;
+        health.OnDie += HandleDie;
+    }
+
+    public void SpendCoins(int costToFire)
+    {
+        TotalCoins.Value -= costToFire;
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (!IsServer) { return; }
+
+        health.OnDie -= HandleDie;
+    }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
@@ -16,5 +49,36 @@ public class CoinWallet : NetworkBehaviour
         if (!IsServer) { return; }
 
         TotalCoins.Value += coinValue;
+    }
+
+    public void HandleDie(Health healt)
+    {
+        int bountyValue = (int)(TotalCoins.Value * (bountyPercentage / 100));
+        int bountyCoinValue = bountyValue / bountyCoinCount;
+
+        if (bountyCoinValue < minBountyCoinValue) { return; }
+
+        for (int i=0; i< bountyCoinCount; i++)
+        {
+            BountyCoin coinInstance = Instantiate(coinPrefab, GetSpawnPoint(), Quaternion.identity);
+            coinInstance.SetValue(bountyCoinValue);
+            coinInstance.NetworkObject.Spawn();
+        }
+    }
+
+    private Vector2 GetSpawnPoint()
+    {
+        while (true)
+        {
+            float xRand = gameObject.transform.position.x + 5;
+            float yRand = gameObject.transform.position.y + 5;
+            Vector2 randPos = new Vector2(xRand, yRand);
+            Vector2 spawnPoint = (Vector2)transform.position + randPos;
+            int numColliders = Physics2D.OverlapCircleNonAlloc(spawnPoint, coinRadius, coinBuffer);
+            if (numColliders == 0)
+            {
+                return spawnPoint;
+            }
+        }
     }
 }
